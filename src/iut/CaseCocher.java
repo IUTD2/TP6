0e3414833f4c7604be52065f679f38081561bd66
baptistePrunot <baptiste.prunot@gmail.com>
2016-05-01 21:53:50 +0100

    ajout de la fonction clic

--- src/iut/CaseCocher.java
+++ src/iut/CaseCocher.java
@@ -1,63 +1,72 @@
 package iut;
 
 /**
- * la case à cocher est un composant à deux états, l'utilisateur change l'état en cliquant dessus. 
+ * la case à cocher est un composant à deux états, l'utilisateur change l'état
+ * en cliquant dessus.
  */
 public class CaseCocher extends Label {
-	/**
-	 * état de la coche
-	 */
-	private boolean etat=false;	
-                        
-        public CaseCocher(Composant parent, int x, int y, int w, int h)
-        {
-            super(parent,x,y,w,h);        
+
+    /**
+     * état de la coche
+     */
+    private boolean etat = false;
+
+    public CaseCocher(Composant parent, int x, int y, int w, int h) {
+        super(parent, x, y, w, h);
+    }
+
+    /**
+     * Effectue un clic : change l'état de la case à cocher
+     * 
+     * @author Baptiste Prunot
+     */
+    @Override
+    protected void clic() {
+        if (this.etat == false) {
+            this.etat = true;
+        } else {
+            this.etat = false;
         }
-        
-        /**
-         * Effectue un clic : change l'état de la case à cocher
-         * @author
-         */
-        @Override
-        protected void clic()
-        {
-            
-        }
-        
-        /**
-         * Dessine la case à cocher : un rectangle (avec une croix si cochée) et du texte
-         * @param e 
-         * @author
-         */
-        @Override
-	public void dessiner(Ecran e) {
+    }
 
-	}
+    /**
+     * Dessine la case à cocher : un rectangle (avec une croix si cochée) et du
+     * texte
+     *
+     * @param e
+     * @author
+     */
+    @Override
+    public void dessiner(Ecran e) {
 
-        @Override
-	protected String nomType() {
-		return "casecocher";
-	}
+    }
 
-	/**
-	 * @return true si le composant peut recevoir des enfants
-	 */
-        @Override
-	protected boolean peutAvoirComposants() {
-		return false;
-	}
+    @Override
+    protected String nomType() {
+        return "casecocher";
+    }
 
-	public void setEtat(boolean etat) {
-		this.etat = etat;
-	}
+    /**
+     * @return true si le composant peut recevoir des enfants
+     */
+    @Override
+    protected boolean peutAvoirComposants() {
+        return false;
+    }
 
-	public boolean isEtat() {
-		return this.etat;
-	}
+    public void setEtat(boolean etat) {
+        this.etat = etat;
+    }
 
-	public boolean sikFunction() {
-		// return this.etat;
-    retrun "REALLY awesome"
+    public boolean isEtat() {
+        return this.etat;
+    }
+
+    public boolean sikFunction() {
+        // return this.etat;
+        retrun 
+
+"REALLY awesome"
 	}
 
 }
